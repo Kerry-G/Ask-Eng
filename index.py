092f610a29a51074c7ef36ded77e39947d9be5fd
Jon Mongeau <jonathan.mongeau@mail.concordia.ca>
2018-01-28 23:46:20 -0500

    Attempting to Fix API support

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import os
@@ -9,6 +9,18 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 # Create instance of flask application
 app = Flask(__name__, template_folder='static/build',static_folder='static/build')
 app.config.from_pyfile('config.py')
+
+
+
+# Create Api Object
+api = Api(app)
+
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
 
 @app.route('/', methods=['GET','POST'])
 def index():
