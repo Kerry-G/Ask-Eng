28b70467950dea23b8c2905f9714d2671a2ec44d
Unknown <jamal.g16@hotmail.com>
2018-02-13 15:44:08 -0500

    User Profile Page Routing
    The profile information changes dynamically according to the user ID in the url. When redirecting to the path of a specific user, the page will fetch the user's profile information based on the id in the url. Still requires formatting, as well as a "Page not Found" for inexistent user IDs.

--- static/src/components/Profile.js
+++ static/src/components/Profile.js
@@ -5,22 +5,51 @@ import ProfileBody from './body/Profile/ProfileBody';
 import {Col, Row, Grid} from 'react-bootstrap'
 import { connect } from 'react-redux'
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import {fetchAPI} from './utility'
 
 
 class Profile extends Component {
   constructor(props) {
     super(props);
+	this.state = {
+		alert: false,
+		email: "",
+		fname: "",
+		lname: "",
+		engineer: "",
+		}
 	}
+	
+	 componentDidMount() {
+      this.fetchUserInfo()
+    }
+
+	//fetches info on the user. Uses the id in the URL to find the user in the database and sets the corresponding state values to be
+	//displayed
+	 async fetchUserInfo(){
+    fetchAPI("GET", "/api/users/" + this.props.match.params.id).then(
+      response => {
+        try{
+          if (response.success){
+		  console.log(response.user)
+				this.setState({alert: false, email: response.user.email, fname: response.user.fname, lname: response.user.lname, engineer: response.user.engineer})
+          }
+          else{
+            this.setState({alert: true})
+
+          }
+        } catch(e){console.error("Error", e)}
+      }
+    ).catch((e)=>console.error("Error:", e))
+  }
 
   render() {
-
   console.log(this.props.match.params);
-
-
-   let ProfileInfo;
-	 ProfileInfo = <div> <h1> {this.props.user.fname} {this.props.user.lname} </h1>
-					<h3>	&nbsp;&nbsp;&nbsp;Discipline: {this.props.user.engineer} engineering <br></br><br></br>
-							&nbsp;&nbsp;&nbsp;Contact information: {this.props.user.email}
+	
+	let ProfileInfo;
+	 ProfileInfo = <div> <h1> {this.state.fname} {this.state.lname} </h1>
+					<h3>	&nbsp;&nbsp;&nbsp;Discipline: {this.state.engineer} engineering <br></br><br></br>
+							&nbsp;&nbsp;&nbsp;Contact information: {this.state.email}
 					</h3>
 					<h4> <br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;User ID: {this.props.match.params.id} </h4>
 	 </div>
