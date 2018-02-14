e2e4bf5124924ac8c1ce6ad012f4484aeb29fffa
Sharon Ho <sharon.cy.ho@gmail.com>
2018-02-13 21:34:00 -0500

    Added logo in header

--- static/src/components/header/Header.js
+++ static/src/components/header/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Col, Row} from 'react-bootstrap'
+import {Col, Row, Image} from 'react-bootstrap'
 import { connect } from 'react-redux'
 import {logOut} from '../../store/auth'
 class Header extends Component {
@@ -9,12 +9,14 @@ class Header extends Component {
     if (!(Object.keys(this.props.user).length === 0 && this.props.user.constructor === Object)){ //if the user is connected
       user = <p className="header-name"> Hello, {this.props.user.fname} {this.props.user.lname}&nbsp;|&nbsp;&nbsp;<a className="logoutBtn" onClick={logOut}>log out</a></p>
     }
+    let image;
+      image = "images\\ask-eng.png"
     return (
         <Row className="header">
-          <Col xs={8} >
-              <h1>Ask&nbsp;<small>Eng</small></h1>
+          <Col xs={6}>
+            <h1><Image src={image} width={64} circle />&nbsp;Ask&nbsp;<small>Eng</small></h1>
           </Col>
-          <Col xsHidden md={4}>
+          <Col xs={6}>
             {user}
           </Col>
         </Row>
