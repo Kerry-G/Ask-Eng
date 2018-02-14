3f629e3ab382fd1c0c8dc8ab618c4d0887ef6157
Kerry Gougeon Ducharme <kerrygougeon@gmail.com>
2018-02-13 19:18:12 -0500

    deleted table off profile card

--- static/src/components/body/LoginBox/Profile.js
+++ static/src/components/body/LoginBox/Profile.js
@@ -4,34 +4,27 @@ import { connect } from 'react-redux'
 
 class Profile extends Component{
   render(){
+    let points = {
+      float: "right"
+    }
     return(
       <Panel bsStyle="primary">
         <Panel.Heading>
-          <Panel.Title componentClass="h3">{this.props.user.fname} {this.props.user.lname}'s Profile</Panel.Title>
+          <Panel.Title componentClass="h3">{this.props.user.fname}&nbsp;{this.props.user.lname}'s Profile</Panel.Title>
         </Panel.Heading>
-          <ListGroup>
-            <ListGroupItem>
+        <Panel.Body>
               <Glyphicon glyph="user" />&nbsp;&nbsp;&nbsp;&nbsp;{this.props.user.engineer} engineering
               <br />
               <Glyphicon glyph="envelope" />&nbsp;&nbsp;&nbsp;&nbsp;{this.props.user.email}
               <br />
               <Glyphicon glyph="cog" />&nbsp;&nbsp;&nbsp;&nbsp;my settings
-              <Table>
-                <tr>
-                  <td>
-                    <Glyphicon glyph="comment" />&nbsp;&nbsp;&nbsp;&nbsp;my questions
-                    <br />
-                    <Glyphicon glyph="comment" />&nbsp;&nbsp;&nbsp;&nbsp;my answers
-                  </td>
-                  <td>
-                    <Glyphicon glyph="thumbs-up" />&nbsp;&nbsp;&nbsp;&nbsp;{this.props.user.ups}
-                    <br />
-                    <Glyphicon glyph="thumbs-down" />&nbsp;&nbsp;&nbsp;&nbsp;{this.props.user.downs}
-                  </td>
-                </tr>
-              </Table>
-            </ListGroupItem>
-          </ListGroup>
+              <br />
+              <Glyphicon glyph="comment" />&nbsp;&nbsp;&nbsp;&nbsp;my questions
+              <div style={points}><Glyphicon glyph="thumbs-up" />&nbsp;&nbsp;&nbsp;&nbsp;{this.props.user.ups}</div>
+              <br />
+              <Glyphicon glyph="comment" />&nbsp;&nbsp;&nbsp;&nbsp;my answers
+              <div style={points}><Glyphicon glyph="thumbs-down" />&nbsp;&nbsp;&nbsp;&nbsp;{this.props.user.downs}</div>
+          </Panel.Body>
       </Panel>
     );
   }
