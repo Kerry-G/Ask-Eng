704edcb5a5f0f0121c7b72416ebf42f310cff1a8
Nico Hudon <nico.tide2x@gmail.com>
2018-03-07 11:00:18 -0500

    fixed user modify and added tests

--- test.py
+++ test.py
@@ -15,25 +15,57 @@ def test_questions():
 
 def test_answers():
     Answers.createAnswer("My Answer", 3, 10)
-    Answers.getAnswersByQuestion(10)
-
-
-def test_users():
-    Users.createUser("Marc", "Hamill", "starwarsfan", "wookie", "Space", "chewbaca", True)
-    user = Users.getUser("starwarsfan")
-    if user:
+    answer = Answers.getAnswersByQuestion(10)
+    if answer:
         return 0
     else:
         return 1
+
+
+def test_users():
+    # test with non existing user
+    if Users.userExists("starwarsfan"):
+        return 1
+    if Users.userVerified("starwarsfan", "wookie"):
+        return 2
+    if Users.modifyUser(1, "Boba", "Fett", "Killer", "guns"):
+        return 3
+
+    # test with existing user
+    Users.createUser("Mark", "Hamill", "starwarsfan", "wookie", "Space", "chewbaca", True)
+    if not Users.userExists("starwarsfan"):
+        return 4
+    if not Users.userVerified("starwarsfan", "wookie"):
+        return 5
+    if Users.userVerified("starwarsfan", "guns"):
+        return 6
+
+    user = Users.getUser("starwarsfan")
+    if not user:
+        return 7
+    if user["fname"] != "Mark" or user["lname"] != "Hamill" or user["engineer"] != "Space" \
+            or user["display_image"] != "chewbaca":
+        return 8
+    if user["password_hash"] == "wookie":
+        return 9
+    if not Users.modifyUser(user["id"], "Han", "Solo", "Ship", "leia"):
+        return 10
+    user = Users.getUser("starwarsfan")
+    if user["fname"] != "Han" or user["lname"] != "Solo" or user["engineer"] != "Ship" \
+            or user["display_image"] != "leia":
+        return 11
+    if user["password_hash"] == "wookie":
+        return 12
+
+    return 0
 
 
 # Create instance of flask application
 app = Flask(__name__, template_folder='static/build', static_folder='static/build')
 app.config.from_pyfile('test_config.py')
 
-# Delete db
-if os.path.exists(os.path.dirname(os.path.abspath(__file__)) + "\\test.db"):
-    os.remove(os.path.dirname(os.path.abspath(__file__)) + "\\test.db")
+# empty db
+open(os.path.dirname(os.path.abspath(__file__)) + "\\test.db", 'w').close()
 
 # Create db
 db = SQLAlchemy(app)
@@ -47,11 +79,15 @@ if __name__ == '__main__':
 
     return_code = test_users()
     if return_code != 0:
-        print("backend Test failed at User")
+        print("backend Test failed at User, Rc=" + str(return_code))
         exit(return_code)
     return_code = test_questions()
     if return_code != 0:
-        print("backend Test failed at Question")
+        print("backend Test failed at Question, Rc=" + str(return_code))
+        exit(return_code)
+    return_code = test_answers()
+    if return_code != 0:
+        print("backend Test failed at Question, Rc=" + str(return_code))
         exit(return_code)
     print("Backend Test completed")
     exit(0)
