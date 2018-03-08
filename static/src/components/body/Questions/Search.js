bbf81a8ff1f3886dd28ef8a7e05b4a7d14e9a6a4
Jon Mongeau <jonathan.mongeau@mail.concordia.ca>
2018-03-07 21:34:31 -0500

    Made Search.js not crash

--- static/src/components/body/Questions/Search.js
+++ static/src/components/body/Questions/Search.js
@@ -3,16 +3,24 @@ import { Form, FormControl, Button} from 'react-bootstrap'
 import FontAwesome from 'react-fontawesome' 
 
 class Search extends Component{
+
+
+
+
+
+
     render() {
 
 
         return  (
             <Form >
-            <FormControl className ="search"  bsSize="sm" type="email" onChange={(e)=>{this.setState({email:e.target.value})}} placeholder="enter your search" />
-            <button className="reg-btn-color" id="searchButton"  onClick={(e)=>this.handleLogin(e)}>{<FontAwesome name='fas fa-search' />}</button>
+            <FormControl className ="search"  bsSize="sm" type="text" onChange={(e)=>{this.setState({email:e.target.value})}} placeholder="enter your search" />
+            <button className="reg-btn-color" id="searchButton"  onClick={(e)=>this.handleSearch(e)}>{<FontAwesome name='fas fa-search' />}</button>
             </Form> 
           
-    )       
+    )
+
+
     }
 }
 
